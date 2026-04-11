b2dc8920c1f4144b6e31149e9446204f953dba12
Michael <michael.ramos@epn.edu.ec>
2026-04-11 07:18:04 -0500

    Se mejoro el texto de bienvenida

--- prjgithub/hi.java
+++ prjgithub/hi.java
@@ -1,5 +1,5 @@
 public class hi {
     public static void main(String[] args) {
-        System.out.println("Hola bienvenido a Java");
+        System.out.println("¡Bienvenido al mundo de Java, Estás por descubrir uno de los lenguajes más potentes y utilizados en la industria, donde cada línea de código que escribas te ayudará a entender cómo se construyen los grandes sistemas de información. Al principio puede parecer estricto por sus reglas y su estructura, pero una vez que domines la lógica de clases y objetos, tendrás el poder de crear aplicaciones robustas y escalables. ¡Mucho éxito en este camino en Programación II!");
     }
 }
